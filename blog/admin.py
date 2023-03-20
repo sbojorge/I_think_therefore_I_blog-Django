a2572d1524f63e6475d78f01f055d9918857891f
sbojorge <sara.bojorge@gmail.com>
2023-03-20 16:40:57 +0000

    Add the comment model to the admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content',)
 
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approved_comment']
+
+    def approved_comment(self, request, queryset):
+        queryset.update(approved=True)
+        
 
