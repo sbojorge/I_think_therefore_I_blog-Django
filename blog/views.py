f7635d6354ae3ba4f60d44ed16fa3e68954f6723
sbojorge <sara.bojorge@gmail.com>
2023-03-23 15:48:28 +0000

    add messages

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .forms import CommentForm
 from django.http import HttpResponseRedirect
 
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
